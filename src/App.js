de78d676b2db9c75084fb8918756e5682dde9bfb
Daniel Cheong <dcheo001@gmail.com>
2022-01-05 19:31:56 -0500

    finished with basic layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-// import Home from "./pages/home/Home";
+import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 import Settings from "./pages/settings/Settings";
-// import Single from "./pages/single/Single";
-// import Write from "./pages/write/Write";
-
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Switch, Route, Linked } from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
-    <>
+    <Router>
       <TopBar />
-      <Settings />
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">{user ? <Home/> : <Register />}</Route>
+        <Route path="/login">{user ? <Home /> : <Login />}</Route>
+        <Route path="/write">{user ? <Write /> : <Register />}</Route>
+        <Route path="/settings">{user ? <Settings /> : <Register />}</Route>
+        <Route path="/post/:postId">
+          <Single />
+        </Route>
+      </Switch>
+     
+    </Router>
   );
 }
 
