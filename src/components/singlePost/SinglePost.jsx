dfa3431837f0263f4220263343f8e6cbde09aef6
Peter Lamson <ptrlmsn@gmail.com>
2022-01-07 13:13:18 -0500

    Theres now states to switch between editing and finishing editing - the CSS is kinda wonky on the editing boxes though? Also, the forms for title and content edits are both controlled for when we do the actual patching.

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -7,6 +7,9 @@ import { useHistory } from "react-router-dom"
 export default function SinglePost() {
     let history = useHistory()
     const [postState, setPostState] = useState({title: '', content: '', author_id: '', author: {id: '', author_name: ''}})
+    const [editState, setEditState] = useState(false)
+    const [titleEdit, setTitleEdit] = useState('')
+    const [contentEdit, setContentEdit] = useState('')
     const targetId = useParams()
     useEffect(()=> {
         fetch(`http://localhost:9292/posts/${targetId.postId}`)
@@ -32,14 +35,14 @@ export default function SinglePost() {
                 src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
                 alt="" 
                 className="singlePostImg" 
-                />
+                />{editState ? <textArea onChange={(e)=>{setTitleEdit(e.target.value)}}>{postState.title}</textArea> :
                 <h1 className="singlePostTitle">
                     {postState.title}
                 <div className="singlePostEdit">
-                    <i className="singlePostIcon far fa-edit"></i>
+                    <i className="singlePostIcon far fa-edit" onClick={(e)=>{setEditState(true)}}></i>
                     <i className="singlePostIcon far fa-trash-alt" onClick={handleDelete}></i>
                 </div>
-                </h1>
+                </h1>}
                 <div className="singlePostInfo">
                     <span className="singlePostAuthor">
                         Author: <b>{postState.author.author_name}</b>
@@ -47,8 +50,9 @@ export default function SinglePost() {
                     <span className="singlePostDate">{moment(postState.date).format('MMMM DD YYYY,  LT')}
 </span>
                 </div>
+                {editState ? <div><textarea onChange={(e)=>{setContentEdit(e.target.value)}}>{postState.content}</textarea><button onClick={(e)=>{setEditState(false)}}>Submit Changes</button> </div> :
             <p className="singlePostDesc">
-                {postState.content} </p>
+                {postState.content} </p> }
             </div>
         </div>
     )
